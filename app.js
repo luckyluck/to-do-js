b182ed617c6b744ee4e96996a4aa4b22104c9249
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-05-02 09:22:26 +0200

    Adding titles, optionally storing in local storage, initiating with items.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const todo = TD();
+const todo = TD('#todoList', [], { save: true });
 const addButton = document.getElementById('addButton');
 
 addButton.addEventListener('click', function (event) {
   event.preventDefault();
+
   const title = document.getElementById('title');
-  console.log(title.value);
+  if (!title) return;
+  todo.add(title.value);
+  title.value = '';
 });
--- todo.js
+++ todo.js
@@ -1,18 +1,74 @@
-;(function(global) {
+;(function (global) {
+  const todoList = [];
+  let config = {
+    save: false
+  };
+  let container;
+
   // 'new' an object
-  const TD = function () {
-    return new TD.init();
+  const TD = function (containerSelector = '#list', titles = [], options = {}) {
+    return new TD.init(containerSelector, titles, options);
   };
 
   // prototype holds methods (to save memory space)
-  TD.prototype = {};
+  TD.prototype = {
+    mount: function () {
+      const savedItems = JSON.parse(localStorage.getItem('myTodo') || '[]');
+      if (savedItems.length > 0) {
+        this.add(...savedItems);
+      }
+    },
+
+    add: function (...titles) {
+      this.validate(titles);
+
+      todoList.push(...titles);
+      this.save();
+      this.draw();
+    },
+
+    save: function () {
+      if (config.save) {
+        localStorage.setItem('myTodo', JSON.stringify(todoList));
+      }
+    },
+
+    draw: function () {
+      console.log(todoList);
+      // TODO draw items
+    },
+
+    validate: function (titles) {
+      if (titles.length === 0) {
+        throw 'Cannot add empty item';
+      }
+    }
+  };
 
   // the actual object is created here, allowing us to 'new' an object without calling 'new'
-  TD.init = function () {};
+  TD.init = function (containerSelector, titles, options) {
+    container = document.querySelector(containerSelector);
+    if (!container) {
+      throw 'TODO items container should be provided';
+    }
+
+    this.mount();
+
+    config = {
+      ...config,
+      ...options
+    };
+
+    // If we have provided items on init
+    // we should add them to the list
+    if (titles.length > 0) {
+      this.add(...titles);
+    }
+  };
 
   // trick borrowed from jQuery so we don't have to use the 'new' keyword
   TD.init.prototype = TD.prototype;
 
   // attach our TD to the global object, and provide a shortand '$G' for ease our poor fingers
   global.TD = TD;
-}(window))
+}(window));
